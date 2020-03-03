d90cf585d48bc192f1766fa9062e20a2a9f5b071
Adrian Oeftiger <a.oeftiger@gsi.de>
2020-03-03 14:34:24 +0100

    Version 1.0.0 with updated README

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     name='FITX',
     version=v,
     description='Fit my InsTability eXponential',
-    long_description='A library to isolate and fit exponential rise times in unstable systems with saturation.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     url='https://github.com/aoeftiger/FITX',
     author='Adrian Oeftiger',
     author_email='adrian@oeftiger.net',
@@ -19,5 +20,12 @@ setup(
     install_requires=['numpy'],
     include_package_data=True,
     zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=2.6",
 )
-
